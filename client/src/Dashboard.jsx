eeffe83009de259d05ebe2a697c8b9b1c154d6dd
AnujBora2022 <121304516+AnujBora2022@users.noreply.github.com>
2026-05-26 18:58:04 +0530

    Updtaed API

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -12,7 +12,8 @@ import { ProjectModal, MemberModal, IssueModal } from "./components/dashboard/Mo
 import { UsersIcon, IssueIcon, CommentIcon, FolderIcon } from "./components/dashboard/icons";
 import { styles } from "./components/dashboard/styles";
 
-const API = "http://localhost:5000";
+// const API = "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL;
 const api = () => {
   const token = localStorage.getItem("token");
   return axios.create({ baseURL: API, headers: { Authorization: `Bearer ${token}` } });
